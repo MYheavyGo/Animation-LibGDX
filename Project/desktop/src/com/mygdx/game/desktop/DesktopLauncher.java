2c1abc620df3546fdfbca1d16e9812f1a45b2547
MYheavyGo <ga.sprunger@gmail.com>
2015-05-15 22:59:31 +0200

    Project clean and build / Yeah

---
+++ Project/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.mygdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.game.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+	    LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+	    config.title = "Animation - LibGDX";
+	    config.useGL30 = true;
+	    config.fullscreen = false;
+	    LwjglApplication lwjglApplication = new LwjglApplication(new Main(), config);
+	}
+}
